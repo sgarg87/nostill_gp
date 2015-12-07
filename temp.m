eb4ffa4ee4384b5a1933a7dfc57e44efc63409e7
Sahil Garg <sahilgar@usc.edu>
2015-12-06 20:16:25 -0800

    removing the image data

--- temp.m
+++
@@ -1,9 +0,0 @@
-close(gcf);
-for i=1:100; 
-    for j=1:100; 
-        if i~= j 
-            plot( imageData.data(:,i), imageData.data(:,j), 'kx' ); 
-            pause(1); 
-        end
-    end
-end
